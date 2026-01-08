0beff34985b426c481fc2945f6b9ab8f17d35f70
Open Numerics <info@opennumerics.com>
2026-01-07 22:49:36 -0500

    Making text shorter

--- experience.js
+++ experience.js
@@ -33,7 +33,7 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       title: "Fast Exploration of Physical Designs using Generative AI",
       link: "/solutions/sgm.html",
-      description: "The first Industrial Case of Score-based Generative Models for Computational Science",
+      description: "The first Industrial GenAI Model for Computational Science",
       image: "images/sgm/sgm_tile.png"
     },
   ];
